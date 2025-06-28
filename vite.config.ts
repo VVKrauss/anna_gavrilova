b50fce5e8c13fbb2119b19b225d9e6d8da2a258c
VVKrauss <112692988+VVKrauss@users.noreply.github.com>
2025-06-28 02:37:02 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   root: '.',
   build: {
-    outDir: 'dist',
+    outDir: 'build',
     emptyOutDir: true,
     rollupOptions: {
       input: resolve(__dirname, 'index.html'),
