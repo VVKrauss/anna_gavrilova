b9ee66f0bd903acb5d70b8f127564b4e37c8fa5c
VVKrauss <112692988+VVKrauss@users.noreply.github.com>
2025-07-02 19:44:05 +0200

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import App from './App.tsx';  // Временно отключено
-import { TestPage } from './TestPage.tsx';  // Временно включено для диагностики
+import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TestPage />
+    <App />
   </StrictMode>
-);
+);  
